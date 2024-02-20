aa267e2d35abbe755a9b5dfdc5162814b6212a18
olp <t.miyai0001@gmail.com>
2024-02-20 16:03:49 +0900

    環境変数の作成(本番URLなし)

---
+++ env.development.ts
@@ -0,0 +1,5 @@
+export default {
+    APP_ENV: {
+      DEV: 'http://local.modeBuy'
+    }
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+const defineNuxtConfig = async () => {
+  
+  return {
+    devtools: { enabled: true },
+    
+    runtimeConfig: {
+      
+    }
+  }
+}
+export default defineNuxtConfig
+
+
